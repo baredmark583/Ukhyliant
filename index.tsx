b11c117597719d1de5742e6ab490fb2bbae35d2d
baredmark583 <baredmark583@gmail.com>
2025-08-11 15:28:02 +0300

    Initial setup for Render

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@tabler/core/dist/css/tabler.min.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
