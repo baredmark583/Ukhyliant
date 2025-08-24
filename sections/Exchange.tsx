f2bbe3362330b15947ab4c97d56038c6c76697f4
baredmark583 <baredmark583@gmail.com>
2025-08-24 21:22:41 +0300

    Initial setup for Render

--- sections/Exchange.tsx
+++ sections/Exchange.tsx
@@ -244,7 +244,7 @@ const ExchangeScreen: React.FC<ExchangeProps> = ({ playerState, currentLeague, o
                     <img
                         src={coinSkinUrl}
                         alt="coin"
-                        className={`col-start-1 row-start-1 w-full h-full rounded-full transition-all duration-300 pointer-events-none ${isTurboActive ? 'animate-pulse' : ''}`}
+                        className={`col-start-1 row-start-1 w-[90%] h-[90%] rounded-full transition-all duration-300 pointer-events-none ${isTurboActive ? 'animate-pulse' : ''}`}
                         style={isTurboActive ? { filter: 'drop-shadow(0 0 20px #f59e0b)' } : {}}
                         draggable="false"
                         {...(isExternal(coinSkinUrl) && { crossOrigin: 'anonymous' })}
@@ -254,7 +254,7 @@ const ExchangeScreen: React.FC<ExchangeProps> = ({ playerState, currentLeague, o
                             src={getProxiedUrl(leagueOverlayUrl)}
                             onError={handleOverlayError}
                             alt="league effect"
-                            className="col-start-1 row-start-1 w-full h-full pointer-events-none"
+                            className="col-start-1 row-start-1 w-[90%] h-[90%] pointer-events-none"
                             draggable="false"
                         />
                     )}
